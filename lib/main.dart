4023834acdd58b9ced485f30a9641103bae0c888
Abood <sayyedabood69@gmail.com>
2022-02-01 18:42:52 +0530

    Added ScrollView and Divided Widgets into mulitple widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
+//                         UserTransaction
+//                             |returning column
+//                            | holding
+//           --------------------------------
+//         |                                |
+//    NewTransaction()              TransactionList(_userTransactions)
+//           |                                   |
+//         Card(                            Card(
+//           TextField                        Row[
+//           TextField                             Displaying List(Title nd amount)
+//           FlatButton                           ]
+//         )                                )
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-
-import './transaction.dart';
+import './widgets/user_transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,94 +26,25 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  List<Transaction> transactions = [
-    Transaction(
-        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    Transaction(
-        id: 't2',
-        title: 'Weekly Groceries',
-        amount: 16.53,
-        date: DateTime.now()),
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Column(
-          // mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Text('Chart 1'),
-                color: Colors.blue,
-                elevation: 5,
-              ),
-            ),
-            Card(
+      body: SingleChildScrollView(   // To make list scrollable
+        child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('Chart 1'),
+              color: Colors.blue,
               elevation: 5,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                          labelText: 'Title',
-                          labelStyle: TextStyle(color: Colors.deepOrange)),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          labelText: 'Amount',
-                          labelStyle: TextStyle(color: Colors.deepOrange)),
-                    ),
-                    FlatButton(
-                      onPressed: () {},
-                      child: Text('Add Transaction'),
-                      textColor: Colors.purple,
-                    )
-                  ],
-                ),
-              ),
             ),
-            Column(
-              children: transactions.map((tx) {
-                return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                          margin: EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 15),
-                          decoration: BoxDecoration(
-                              border:
-                                  Border.all(color: Colors.purple, width: 2)),
-                          padding: EdgeInsets.all(10),
-                          child: Text('\$ ${tx.amount}',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                  color: Colors.purple))),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(tx.title,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 16)),
-                          Text(
-                            DateFormat.yMMMd().format(tx.date),
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              }).toList(),
-            )
-          ]),
+          ),
+          UserTransactions()
+        ]),
+      ),
     );
   }
 }
