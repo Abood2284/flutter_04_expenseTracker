60281f4fae6401a7110423e48fd177a007c40583
Abood <sayyedabood69@gmail.com>
2022-02-02 19:45:23 +0530

    ListView.builder building dynamic list based on the Data available

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,5 +1,5 @@
-// This widget file holds Card which is responsible for taking Transaction List through it's constructor.
-// And display it on the screen using dynamic .map() metho
+// ! This widget file holds Card which is responsible for taking Transaction List through it's constructor.
+// ! And display it on the screen using dynamic .map() metho
 
 // ignore_for_file: prefer_const_constructors
 
@@ -12,12 +12,14 @@ class TransactionList extends StatelessWidget {
   List<Transaction> transactions;
 
   TransactionList(this.transactions);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 300,
       child: ListView.builder(
-        // We defined it's parent(Container) height 300 becoz, ListView default takes infinite space resulting in no widgets rendered
+        // ! We defined it's parent(Container) height 300 becoz, ListView default takes infinite space resulting in no widgets rendered
+        // * Gives the context[we dont need] also give the index of the current element at pos
         itemBuilder: (context, index) {
           return Card(
             child: Row(
@@ -25,26 +27,23 @@ class TransactionList extends StatelessWidget {
                 Container(
                     margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                     decoration: BoxDecoration(
-                        border: Border.all(color: Colors.purple, width: 2)),
+                        border: Border.all(
+                          color: Theme.of(context).primaryColor,
+                          width: 2)),
                     padding: EdgeInsets.all(10),
-                    child: Text('\$ ${transactions[index].amount.toStringAsFixed(2)}', // so that only upto 2 decimal places are showed
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple))),
+                    child: Text(
+                        '\$ ${transactions[index].amount.toStringAsFixed(2)}', // * only upto 2 decimal places are showed
+                        style: Theme.of(context).textTheme.headline6)),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(transactions[index].title,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16)),
+                        style: Theme.of(context).textTheme.headline6),
                     Text(
                       DateFormat.yMMMd().format(transactions[index].date),
                       style: TextStyle(color: Colors.grey),
-                    ),
-                  ],
-                )
-              ],
+                    )],
+                )],
             ),
           );
         },
