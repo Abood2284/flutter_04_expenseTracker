67f4518b074b035531d6d382c2b8d960350357e3
Abood <sayyedabood69@gmail.com>
2022-02-04 20:14:55 +0530

    added if-else section in return of build mehtod

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -17,38 +17,56 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        // ! We defined it's parent(Container) height 300 becoz, ListView default takes infinite space resulting in no widgets rendered
-        // * Gives the context[we dont need] also give the index of the current element at pos
-        itemBuilder: (context, index) {
-          return Card(
-            child: Row(
+      child: transactions.isEmpty
+          ? Column(
               children: [
+                Text(
+                  'No tranasctions added yet',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(
+                  height: 20,
+                ), // * TO get space between 2 widgets
                 Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Theme.of(context).primaryColor,
-                          width: 2)),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                        '\$ ${transactions[index].amount.toStringAsFixed(2)}', // * only upto 2 decimal places are showed
-                        style: Theme.of(context).textTheme.headline6)),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(transactions[index].title,
-                        style: Theme.of(context).textTheme.headline6),
-                    Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                      style: TextStyle(color: Colors.grey),
-                    )],
-                )],
+                    height: 200,
+                    child: Image.asset('assets/images/waiting.png'))
+              ],
+            )
+          : ListView.builder(
+              // ! We defined it's parent(Container) height 300 becoz, ListView default takes infinite space resulting in no widgets rendered
+              // * Gives the context[we dont need] also give the index of the current element at pos
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                          margin: EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 15),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Theme.of(context).primaryColor,
+                                  width: 2)),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                              '\$ ${transactions[index].amount.toStringAsFixed(2)}', // * only upto 2 decimal places are showed
+                              style: Theme.of(context).textTheme.headline6)),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(transactions[index].title,
+                              style: Theme.of(context).textTheme.headline6),
+                          Text(
+                            DateFormat.yMMMd().format(transactions[index].date),
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+              itemCount: transactions.length,
             ),
-          );
-        },
-        itemCount: transactions.length,
-      ),
     );
   }
 }
