9ad8a45b7ccb9b5e321dbd9b984b43d035bf29f6
abood <sayyedabood69@gmail.com>
2022-02-20 12:59:41 +0530

    Responsible for dynamically generating transaaction lists via constructor of transacitem

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -41,14 +41,21 @@ class TransactionList extends StatelessWidget {
               ],
             );
           })
-        : ListView.builder(
-            // ! 🛑 ListView default takes infinite space, Iniitalize height to parent carefully
-            // * Gives the context[we dont need] also give the index of the current element at pos
-            itemBuilder: (_context, index) {
-              // Here our transaction_item needs only 1 transaction so we are only passing one transaction at our index
-              return TransactionItem(transaction: transactions[index], deleteTx: deleteTx);
-            },
-            itemCount: transactions.length,
-          );
+        : ListView(
+            children: transactions
+                .map(
+                  (tx) => TransactionItem(key: ValueKey(tx.id),deleteTx: deleteTx, transaction: tx),
+                )
+                .toList());
+                /// ! Because key have a bug with ListView.builder. though for long lists we suggest use ListView.builder, here it is exception as we need keys to cahnge bg color of our avatar in transaction item.
+    // : ListView.builder(
+    //     // ! 🛑 ListView default takes infinite space, Iniitalize height to parent carefully
+    //     // * Gives the context[we dont need] also give the index of the current element at pos
+    //     itemBuilder: (_context, index) {
+    //       // Here our transaction_item needs only 1 transaction so we are only passing one transaction at our index
+    //       return TransactionItem(key: ValueKey(transactions[index].id) ,transaction: transactions[index], deleteTx: deleteTx);
+    //     },
+    //     itemCount: transactions.length,
+    //   );
   }
 }
