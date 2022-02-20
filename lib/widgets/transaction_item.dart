539a4457d0b29a92e212a55524a6620679ea857d
abood <sayyedabood69@gmail.com>
2022-02-20 12:58:31 +0530

    holds the card which displays the single transaction

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,9 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key? key,
     required this.transaction,
@@ -16,32 +18,53 @@ class TransactionItem extends StatelessWidget {
   final Function deleteTx;
 
   @override
+  State<TransactionItem> createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  late Color _bgColor;
+  @override
+  void initState() {
+    super.initState();
+    const availableColors = [
+      Colors.red,
+      Colors.green,
+      Colors.amber,
+      Colors.blue,
+      Colors.cyanAccent,
+      Colors.pink,
+    ];
+    _bgColor = availableColors[Random().nextInt(availableColors.length)];
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.symmetric(vertical: 7, horizontal: 4),
       elevation: 6,
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _bgColor,
             child: Padding(
                 padding: EdgeInsets.all(5),
                 child:
-                    FittedBox(child: Text('\$${transaction.amount}'))),
+                    FittedBox(child: Text('\$${widget.transaction.amount}'))),
             radius: 30),
         title: Text(
-          transaction.title,
+          widget.transaction.title,
           style: Theme.of(context).textTheme.headline6,
         ),
-        subtitle: Text(DateFormat.yMMMd().format(transaction.date)),
+        subtitle: Text(DateFormat.yMMMd().format(widget.transaction.date)),
         // * 🚀 I want to show button with icon with some more metadata if user devivce widht is greater than 400
         trailing: MediaQuery.of(context).size.width > 400
             ? FlatButton.icon(
-                onPressed: () => deleteTx(transaction.id),
+                onPressed: () => widget.deleteTx(widget.transaction.id),
                 icon: Icon(Icons.delete),
                 textColor: Theme.of(context).errorColor,
                 label: Text('Delete'),
               )
             : IconButton(
-                onPressed: () => deleteTx(transaction.id),
+                onPressed: () => widget.deleteTx(widget.transaction.id),
                 icon: Icon(Icons.delete),
                 color: Theme.of(context).errorColor,
               ),
